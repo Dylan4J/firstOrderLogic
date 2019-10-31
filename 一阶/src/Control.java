c81b40d572ed94a738bd216c284265b0d6a709dc
hf-510 <836138284@qq.com>
2019-10-31 14:03:57 +0800

    新增了开发者日志

--- 一阶/src/Control.java
+++ 一阶/src/Control.java
@@ -141,13 +141,25 @@ public class Control {
                         sportsman.add(newNode);
                         //2、如果结论里面包含“选择”字眼，则咨询一次，结论库中不添加此类结论
                         if (newConclusion.indexOf("选") == 0 && newConclusion.contains("或")) {
+//                            int a = newConclusion.indexOf("择") + 1;
+//                            int b = newConclusion.indexOf("或");
+//                            int c = newConclusion.length();
                             int a = newConclusion.indexOf("择") + 1;
-                            int b = newConclusion.indexOf("或");
-                            int c = newConclusion.length();
-                            String j = newConclusion.substring(a, b);
-                            String k = newConclusion.substring(b + 1, c);
-                            ask("请问您是要选择" + j + "还是" + k + "呢(选择前者请输入y，选择后者请输入n)");
-                        } else  {
+                            int b = newConclusion.length();
+                            String substring = newConclusion.substring(a,b);
+                            String[] choices = substring.split("或");//结论中给出的选项
+                            StringBuilder choice = new StringBuilder("");
+                            choice.append("请问您是要选择");
+                            for (int j = 0; j < choices.length; j++) {
+                                if (j == choices.length - 1) {
+                                    choice.append(choices[j]);
+                                } else {
+                                    choice.append(choices[j] + "还是");
+                                }
+                            }
+                            choice.append("呢(选择第一项请输入1、选择第二项请输入2....以此类推,多项输入请按照格式 1--2--3 输入)");
+                            ask(choice.toString());
+                        } else {
                             //3、如果结论中不含有“选择”字眼，结论库添加此结论
                             conclusion.add(currentConlusion);
                         }
@@ -209,25 +221,27 @@ public class Control {
      *与咨询者交互的时候询问信息
      * @param string 系统问的话
      */
-    public  void getInf(String string) {
+    public  boolean getInf(String string) {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        String feedcack = scanner.nextLine();
+        boolean inputR = true;//记录咨询者每一次回答是否正常
         int a = 0;
         int b = 0;
         int c = 0;
         /*
         系统询问的方式有两种，一种为：“您是否xxx呢”
-        另外一种为：“您是要选择xxx还是xxx呢”
+        另外一种为：“请问您是要选择xxx还是xxx呢”
          */
-        if (string.contains("您是否")) {
+        if (string.contains("您是否")) {//“您是否xxx呢”
             a = string.indexOf("否") + 1;
             b = string.indexOf("呢");
-
-            if (s.equals("y")) {
-                this.addClientState(string.substring(a,b), true);
-            } else if (s.equals("n")) {
-                this.addClientState(string.substring(a,b), false);
+            String str = string.substring(a,b);
+            if (feedcack.equals("y")) {
+                this.addClientState(str, true);
+            } else if (feedcack.equals("n")) {
+                this.addClientState(str, false);
             } else {
+                inputR = false;
                 try {
                     throw new Exception("输入有误！");
                 } catch (Exception e) {
@@ -236,24 +250,36 @@ public class Control {
                 }
             }
 
-        } else if (string.contains("您是要选择")) {
+        } else if (string.contains("您是要选择")) {//“您是要选择xxx还是xxx还是xxx还是xxx呢”
             a = string.indexOf("择") + 1;
             b = string.indexOf("呢");
-            c = string.indexOf("还");
-
-            if (s.equals("y")) {
-                this.addClientState(string.substring(a, c), true);
-            } else if (s.equals("n")) {
-                this.addClientState(string.substring(c + 2, b), false);
-            } else {
-                try {
-                    throw new Exception("输入有误！");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println("您输入有误，请重新输入！");
+            String choice = string.substring(a,b);
+            String[] choices = choice.split("还是");
+            String[] mulChoice = feedcack.split("--");//存储的用户输入的选项
+            int[] feedbackNum = new int[mulChoice.length];
+            for (int i = 0; i < mulChoice.length; i++) {
+                feedbackNum[i] = Integer.parseInt(mulChoice[i]);
+                if (feedbackNum[i] <= choices.length && feedbackNum[i] > 0) {
+                    this.addClientState(choices[feedbackNum[i]-1], true);
+                } else  {//输入了指定数字以外的内容
+                    System.out.println("是否觉得当前选项不满足您的要求呢（请输入y/n，如果您是输入错误，请输入n）");
+                    inputR = getInf("是否觉得当前选项不满足您的要求呢（请输入y/n，如果您是输入错误，请输入n）");
                 }
             }
+
+        } else if (string.contains("觉得当前选项不满足您的要求")) {
+            if (feedcack.equals("y")) {
+                System.out.println("非常抱歉，keep健身教练目前知道的还比较少，暂时不能就当前问题给您足够的建议呢");
+            } else if (feedcack.equals("n")) {
+                inputR = false;
+                System.out.println("接下来我再问您一遍哦，这一次您可要看清楚了再进行输入哦");
+            } else {//输入了非“y/n”以外的字母
+                System.out.println("您输入有误，请重新输入！");
+                inputR = false;
+            }
         }
+
+        return inputR;
 
 
     }
@@ -263,8 +289,13 @@ public class Control {
      * @param string 系统问的话
      */
     public void ask(String string) {
-        System.out.println(string);
-        getInf(string);
+        while (true) {
+            System.out.println(string);
+            boolean b = getInf(string);
+            if (b) {//询问后咨询者返回的值在正常范围内才跳出循环，否则重复询问
+                break;
+            }
+        }
     }
 
 
